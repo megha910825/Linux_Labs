ce9b002e9ee290234101933dddcfae281db79320
MeghaBansal <megha.bansal@zeiss.com>
2025-06-25 13:35:49 +0200

    first commit

---
+++ dns_domain.sh
@@ -0,0 +1,13 @@
+## Ip address of the DNS Server used in this system
+cat /etc/resov.conf
+
+## command to check the order of dns file and host file 
+cat /etc/nsswitsch.conf
+
+## command to bring up network interface
+sudo ip link set dev eth0 up
+
+## command ro add default route
+
+sudo ip r default via 172.16.238.1
+
---
+++ vi_editor.sh
@@ -0,0 +1,24 @@
+## Vi editor linux lab
+
+## command to check default editor in ubuntu
+update-alternatives --display editor
+
+## Operations mode in vi editor
+- command mode
+- insert mode
+- last line mode
+
+## default mode of vi editor
+- command mode
+
+## vi command to copy a line in vi editor
+yy
+
+## command to delete line in vi editor
+dd
+
+## undo the changes
+u
+
+## Redo the changes
+ctrl + r
